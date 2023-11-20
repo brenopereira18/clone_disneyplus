633370bb1d26fc6345c7043c031ef00641087246
Breno Pereira <brenopereirabetti@gmail.com>
2023-11-20 14:54:24 -0300

    setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function styles() {
+    gulp.src('./src/styles/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./dist/styles'))
+}
+
+exports.default = styles
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) 
+}
