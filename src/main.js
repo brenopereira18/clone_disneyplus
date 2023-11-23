8356c05baae3326a62fc2f5652c592add7156b17
Breno Pereira <brenopereirabetti@gmail.com>
2023-11-23 14:38:10 -0300

    criação da seção 'Perguntas frequentes'(ainda não finalizada)

--- src/main.js
+++ src/main.js
@@ -1,29 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]') /* pega todos os elementos com esse atributo */
-
+    const questions = document.querySelectorAll('[data-faq-question]')
+        
     /* para cada elemento que for clicado, será executado a função */
-    buttons.forEach(this.addEventListener('click', function(botao) {
+    buttons.forEach(addEventListener('click', function(botao) {
         const abaAlvo = botao.target.dataset.tabButton /* pega o valor do data-tab-button do elemento clicado */
         const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`) /* pega o elemento que tem o data-tab-id igual ao do data-tab-button do elemento clicado */
         escondeTodasAsAbas()
         aba.classList.add('shows__list--is-active')
         removeBotaoAtivo()
-        botao.target.classList.add('shows__tabs__button--is-active')    
-    }))   
+        botao.target.classList.add('shows__tabs__button--is-active')            
+    }))
     
-    function removeBotaoAtivo() {
-        const buttons = document.querySelectorAll('[data-tab-button]')   
+    questions.forEach(addEventListener('click', function(element) {
         
-        buttons.forEach((botao) => {
-            botao.classList.remove('shows__tabs__button--is-active')
-        })
-    }
+        const classe = 'faq__questions__item--is-open'
+        const elementoPai = element.target.parentNode
 
-    function escondeTodasAsAbas() {
-        const tabsContainer = document.querySelectorAll('[data-tab-id]')
+        elementoPai.classList.toggle(classe)
+    })) 
 
-        tabsContainer.forEach((tabContainer) => {
-            tabContainer.classList.remove('shows__list--is-active')
-        })        
-    }
 })
+
+/* function abreOuFechaResposta(element) {
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = element.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+} */ 
+    
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]')   
+        
+    buttons.forEach((botao) => {
+        botao.classList.remove('shows__tabs__button--is-active')
+    })
+}
+
+function escondeTodasAsAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]')
+
+    tabsContainer.forEach((tabContainer) => {
+        tabContainer.classList.remove('shows__list--is-active')
+    })        
+}
