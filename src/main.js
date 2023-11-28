7f3c2694baef7f85c9b83b6643fa23f8b0cc68f5
Breno Pereira <brenopereirabetti@gmail.com>
2023-11-28 15:30:53 -0300

    criando o header

--- src/main.js
+++ src/main.js
@@ -2,15 +2,40 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]') /* pega todos os elementos com esse atributo */
     const questions = document.querySelectorAll('[data-faq-question]')
 
-    /* para cada elemento que for clicado, será executado a função */
-    buttons.forEach((botao) => {        
-        botao.addEventListener('click', mudaAba)
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+    
+    window.addEventListener('scroll', () => {
+        const posicaoAtual = window.scrollY      
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader()
+        } else {
+            exibeElementosDoHeader()
+        }
     })
 
+
+    // Seção das atrações
+    buttons.forEach((botao) => {         
+        botao.addEventListener('click', mudaAba) /* para cada elemento que for clicado, será executado a função */
+    })
+
+    // Seção FAQ 
     questions.forEach((question) => {
         question.addEventListener('click', abreEFechaQuestion)
     })
 })
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
 
 function abreEFechaQuestion(element) {
     const classe = 'faq__questions__item--is-open'
