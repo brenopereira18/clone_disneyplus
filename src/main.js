1b52a401d2a786aa8e646bd537576e5ea84b8015
Breno Pereira <brenopereirabetti@gmail.com>
2023-11-28 10:01:07 -0300

    termino da seção 'faq' e ajustes no 'hero'

--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]') /* pega todos os elementos com esse atributo */
     const questions = document.querySelectorAll('[data-faq-question]')
-        
+
     /* para cada elemento que for clicado, será executado a função */
-    buttons.forEach(addEventListener('click', function(botao) {
-        const abaAlvo = botao.target.dataset.tabButton /* pega o valor do data-tab-button do elemento clicado */
-        const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`) /* pega o elemento que tem o data-tab-id igual ao do data-tab-button do elemento clicado */
-        escondeTodasAsAbas()
-        aba.classList.add('shows__list--is-active')
-        removeBotaoAtivo()
-        botao.target.classList.add('shows__tabs__button--is-active')            
-    }))
-    
-    questions.forEach(addEventListener('click', function(element) {
-        
-        const classe = 'faq__questions__item--is-open'
-        const elementoPai = element.target.parentNode
+    buttons.forEach((botao) => {        
+        botao.addEventListener('click', mudaAba)
+    })
 
-        elementoPai.classList.toggle(classe)
-    })) 
-
+    questions.forEach((question) => {
+        question.addEventListener('click', abreEFechaQuestion)
+    })
 })
 
-/* function abreOuFechaResposta(element) {
+function abreEFechaQuestion(element) {
     const classe = 'faq__questions__item--is-open'
     const elementoPai = element.target.parentNode
 
-    elementoPai.classList.toggle(classe)
-} */ 
-    
+    elementoPai.classList.toggle(classe)    
+}
+
+function mudaAba(element) {
+    const abaAlvo = element.target.dataset.tabButton /* pega o valor do data-tab-button do elemento clicado */
+    const aba = document.querySelector(`[data-tab-id = ${abaAlvo}]`) /* pega o elemento que tem o data-tab-id igual ao do data-tab-button do elemento clicado */
+    escondeAba()
+    aba.classList.add('shows__list--is-active') 
+    removeBotaoAtivo()  
+    element.target.classList.add('shows__tabs__button--is-active')    
+}
+
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')   
         
@@ -37,10 +36,10 @@ function removeBotaoAtivo() {
     })
 }
 
-function escondeTodasAsAbas() {
+function escondeAba() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]')
 
     tabsContainer.forEach((tabContainer) => {
         tabContainer.classList.remove('shows__list--is-active')
-    })        
+    })
 }
